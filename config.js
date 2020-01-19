55097321aca6a144c0b3db45ef1ead4350d0d265
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2020-01-20 01:00:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-module.exports = {
-   MONGO_DB: 'mongodb+srv://beny:LxfhXG6nsYmkyFYX@cluster-test-13tia.mongodb.net/merng?retryWrites=true&w=majority',
-   encodingKey : 'this is enconding key'
-}
+
